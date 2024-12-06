8c7446c7076125a1a9825781363c6229a9675f7f
mvelezz <miahvelez7@gmail.com>
2024-12-05 18:12:33 -0700

    Added a MongoDB database and the ability to add and create new members.

--- Artifact One/database.py
+++ Artifact One/database.py
@@ -1,2 +1,25 @@
-from pymongo import MongoClient
+import urllib
 
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+def get_database():
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+
+    username = urllib.parse.quote_plus('miahvelez')
+    password = urllib.parse.quote_plus('Password1')
+    CONNECTION_STRING = "mongodb+srv://"+username+":" + password + "@cluster0.dpolx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    # Create a new client and connect to the server
+    client = MongoClient(CONNECTION_STRING, server_api=ServerApi('1'))
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['CCmembers']
+
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
--- Artifact One_actual/Artifact One/menu.py
+++ Artifact One_actual/Artifact One/menu.py
@@ -3,22 +3,11 @@ The purpose of this file is to define what the menu will look like for the tee t
 """
 
 import member
-from pandas import DataFrame
-
-# Get the database using the method we defined in pymongo_test_insert file
-from database import get_database
-dbname = get_database()
-collection_name = dbname["CCmembers"]
 
 #create a list to hold the member objects
 memberDatabase = []
-
-item_details = collection_name.find()
-for item in item_details:
-    memberDatabase.append(item)
-
 #create a user choices list so users are limited to the available choices
-userChoices = ['1', '2', '3', '4', '5']
+userChoices = ['1', '2', '3', '4']
 
 def getOptions():
     # Print the welcome menu
@@ -27,14 +16,14 @@ def getOptions():
     print("1. Make a tee time")
     print("2. Add a new member")
     print("3. View all members")
-    print("4. Member lookup")
-    print("5. Exit")
+    print("4. Exit")
     user_choice = input()
     #limit the user choices to only those in the menu
     while user_choice not in userChoices:
         print("Please enter a valid option.")
         user_choice = input()
     return user_choice
+
 
 def menu():
     # use a function call to get the user choice
@@ -43,9 +32,9 @@ def menu():
     teeTimes = {}
     memHolder = member.Member
     #Tee times will hold a string as the key and a member object as the value
-    teeTimes["6:00"] = 0
-    teeTimes["7:00"] = 0
-    teeTimes["8:00"] = 0
+    teeTimes["6:00"] = memHolder
+    teeTimes["7:00"] = memHolder
+    teeTimes["8:00"] = memHolder
     #loop forever
     while True:
         if choice == '1':
@@ -53,15 +42,21 @@ def menu():
             for time in teeTimes:
                 memInfo = teeTimes[time]
 
-                print(time, teeTimes[time])
+                print(time, memInfo.memberNumber, memInfo.firstName, memInfo.lastName)
             teeTimeChoice = input("Which tee time would you like to book?")
             print("Which member would you like to add to the tee time?")
             #print all members currently in the member list
+            for membersAvailable in range(len(memberDatabase)):
+                print (membersAvailable)
+                print(memberDatabase[membersAvailable].memberNumber)
+                print(memberDatabase[membersAvailable].firstName)
+                print(memberDatabase[membersAvailable].lastName)
+                print(memberDatabase[membersAvailable].memberStatus)
+                print()
             memberChoice = int(input())
-            item_details = collection_name.find_one({"_id": int(memberChoice)})
+            memberAddingToDatabase = memberDatabase[memberChoice]
             #add member to the tee time
-            print(item_details)
-            teeTimes[teeTimeChoice] = item_details.values()
+            teeTimes[teeTimeChoice] = memberAddingToDatabase
             choice = getOptions()
         elif choice == '2':
             # create a member object
@@ -69,7 +64,7 @@ def menu():
             memberInput = input("Please enter the member number")
             #typcase as the member number is an integer
             int(memberInput)
-            currMember.memberNumber = int(memberInput)
+            currMember.memberNumber = memberInput
             memberInput = input("Please enter the member's first name")
             currMember.firstName = memberInput
             memberInput = input("Please enter the member's last name")
@@ -77,31 +72,11 @@ def menu():
             memberInput = input("Please enter the member's member status")
             currMember.memberStatus = memberInput
             memberDatabase.append(currMember)
-            #query for inserting a member
-            member_to_insert = {
-                "_id": currMember.memberNumber,
-                "first_name": currMember.firstName,
-                "last_name": currMember.lastName,
-                "member_status": currMember.memberStatus
-            }
-
-            collection_name.insert_many([member_to_insert])
             choice = getOptions()
         elif choice == '3':
             #print all members in the list
-            item_details = collection_name.find()
-            for item in item_details:
-                # This does not give a very readable output
-                print(item)
+            for i in range(len(memberDatabase)):
+                print(memberDatabase[i].memberNumber, memberDatabase[i].firstName, memberDatabase[i].lastName, memberDatabase[i].memberStatus )
             choice = getOptions()
-            
         elif choice == '4':
-            memberLookingFor = input("What is the member number you want to search for?")
-
-            #query for the database
-            item_details = collection_name.find_one({"_id" : int(memberLookingFor)})
-            print(item_details)
-            choice = getOptions()
-
-        elif choice == '5':
             break
