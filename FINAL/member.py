1230deaa6d5439a296d112127d57ef99026cbd72
mvelezz <miahvelez7@gmail.com>
2024-12-05 18:24:26 -0700

    Added final Database

---
+++ FINAL/member.py
@@ -0,0 +1,13 @@
+"""
+This file defines the member class. It will allow the menu to create a new member and add them to a list.
+"""
+
+class Member:
+    #int
+    memberNumber = 0
+    #str
+    firstName = None
+    #str
+    lastName = None
+    #str
+    memberStatus = None
